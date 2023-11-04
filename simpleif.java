8aa7ee32a31652a3cb66bc2fe8218263e134e611
Ikuzwesabrine <149713056+Ikuzwesabrine@users.noreply.github.com>
2023-11-04 09:25:48 -0700

    Update simpleif.java

--- simpleif.java
+++ simpleif.java
@@ -1,7 +1,9 @@
-public class simpleif {
+//This program show simpleif
+public class simpleif { // class name
     public static void main(String[] args) {
         int number = 80;
-        if (number == 80) {
+        if (number == 80) { //condition
+           // This is output
             System.out.println("true");
         } else {
             System.out.println("false");
