bf59b248320ee280c4740e3d373d10b15dfa4012
Ikuzwesabrine <149713056+Ikuzwesabrine@users.noreply.github.com>
2023-11-04 07:17:25 -0700

    Update ARTIST.java

--- ARTIST.java
+++ ARTIST.java
@@ -1,6 +1,7 @@
-public class ARTIST{
+//This program show else if condition  with a main method
+public class ARTIST{ //This is class name
     public static void main(String args[]){
-       int artists=45;
+       int artists=45;//This variable
 if(artists<30){
 System.out.println("not allowed to attend boom");
 }
@@ -13,7 +14,7 @@ else if (artists>=50 && artists<60){
 System.out.println("remain one session artists");
 }
 else if (artists>=60 && artists<70){
-   System.out.println("succeed to attend the boom");}
+   System.out.println("succeed to attend the boom");} //This is output
 
    
 }
