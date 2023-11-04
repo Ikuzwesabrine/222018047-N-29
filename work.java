a43d49174a5a608ccd19a29d5fb584399dfbecd7
Ikuzwesabrine <149713056+Ikuzwesabrine@users.noreply.github.com>
2023-11-04 09:36:23 -0700

    Update work.java

--- work.java
+++ work.java
@@ -1,8 +1,9 @@
+//This program show add of two number
 public class work{
     public static void main(String args[]){
         int a=10;
         int b=15;
-if (b>a){
+if (b>a){ //condition
 System.out.println("x:a is great than b");
 }
 else{
