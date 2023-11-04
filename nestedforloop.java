6f879e417cc9dc4e729403c204a4eb37778d4dcf
Ikuzwesabrine <149713056+Ikuzwesabrine@users.noreply.github.com>
2023-11-04 09:07:20 -0700

    Update nestedforloop.java

--- nestedforloop.java
+++ nestedforloop.java
@@ -1,10 +1,12 @@
-public class nestedforloop {
+//This program show nestedforloop
+public class nestedforloop { // class name
      public static void main(String[] args) {
         for(int i = 1; i <= 5; i++){
             for(int j=1; j<=i; j++){
+                // This is output
                 System.out.print (i);
             } 
-            System.out.println();           
+            System.out.println();         
         }
      }
 }
