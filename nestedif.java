15800f48cf5861892b1d8ee5df6914ec1112c4c2
Ikuzwesabrine <149713056+Ikuzwesabrine@users.noreply.github.com>
2023-11-04 09:13:46 -0700

    Update nestedif.java

--- nestedif.java
+++ nestedif.java
@@ -1,7 +1,8 @@
-public class nestedif {
+//This program show nestedif 
+public class nestedif { //class name
     public static void main(String[] args){
         int marks = 4;
-         if (marks>15){
+         if (marks>15){ //condition
             System.out.println("a");
          } 
          else if (marks>10){
