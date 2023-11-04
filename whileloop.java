96a734707fade35b5da9c81a11f915e9d33c5403
Ikuzwesabrine <149713056+Ikuzwesabrine@users.noreply.github.com>
2023-11-04 09:33:33 -0700

    Update whileloop.java

--- whileloop.java
+++ whileloop.java
@@ -1,7 +1,8 @@
+//This program show whileloop
 public class whileloop {
     public static void main(String[] args){
         int i = 1;
-        while(i<=10){
+        while(i<=10){ //condition
             System.out.println("*");
             i++;
         }
